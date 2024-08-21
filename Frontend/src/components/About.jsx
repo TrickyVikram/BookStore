9c4909afddc5561e2ca47381af2cdcb1db7e271c
TrickyVikram <vkumarsah999@gmail.com>
2024-08-21 22:39:23 +0530

    change

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function About() {
+  return (
+   <>
+   <div>
+
+        <div className="mt-28 item-center justify-center text-center">
+         
+         <h1>About us</h1>
+        </div>
+
+   </div>
+   </>
+  )
+}
+
+export default About ;
